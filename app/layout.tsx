8f2769eb899047ac34f8fff9c6a4dc005e949b51
ahad <malikahad017@gmail.com>
2026-07-19 13:48:00 +0500

    Fix accessibility viewport scale issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <style dangerouslySetInnerHTML={{ __html: cssString }} />
       </head>
       <body className="min-h-screen antialiased">
